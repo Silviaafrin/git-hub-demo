b300c4015d10f1942d425047557c71ca7ee93680
silviaafrin <silviafariaqa@gmail.com>
2022-08-12 23:59:32 -0400

    githubdemo

---
+++ src/githubdemo/newClass.java
@@ -0,0 +1,14 @@
+package githubdemo;
+
+public class newClass {
+
+	public newClass() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
